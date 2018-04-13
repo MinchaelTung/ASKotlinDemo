31181cdd25fa2e94a5c64a28b1b466a6c38ff241
Michael Tung <Michael Tung@MICHAEL-N56>
2018-04-13 13:59:06 +0800

    Kotlin 课程 一 二

---
+++ app/src/main/java/com/example/michaeltung/askotlindemo/l1.kt
@@ -0,0 +1,123 @@
+package com.example.michaeltung.askotlindemo
+
+public class l1{
+    //带返回值的方法
+    fun sum1(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun sum2(a: Int, b: Int) = a + b
+    fun sum3(a: Int, b: Int): Int = a + b
+
+    //无返回的方法
+    fun printSum1(a: Int, b: Int): Unit {
+        print(a + b)
+    }
+
+    fun printSum2(a: Int, b: Int) {
+        print(a + b)
+    }
+
+    //可变长参数
+    fun vars(vararg v: Int) {
+        for (vt in v) {
+            print(vt)
+        }
+    }
+
+    //lambda 表达式函数
+    fun lambdaFn(args: Array<String>) {
+
+        val sumLambda: (Int, Int) -> Int = { x, y -> x + y }
+        print(sumLambda(1, 2))
+    }
+//变量
+
+    var a: Int = 1
+    val b = 1       // 系统自动推断变量类型为Int
+    fun aeges() {
+        val c: Int      // 如果不在声明时初始化则必须提供变量类型
+        c = 1           // 明确赋值
+        var x = 5        // 系统自动推断变量类型为Int
+        x += 1           // 变量可修改
+    }
+
+    //字符串模板
+    fun ddd() {
+        // 模板中的简单名称：
+        val s1 = "a is $a"
+        // 模板中的任意表达式：
+        a = 2
+        val s2 = "${s1.replace("is", "was")}, but now is $a"
+    }
+
+    //NULL检查机制
+    //类型后面加?表示可为空
+    var age: String? = "23"
+    //抛出空指针异常
+    val ages = age!!.toInt()
+    //不做处理返回 null
+    val ages1 = age?.toInt()
+    //age为空返回-1
+    val ages2 = age?.toInt() ?: -1
+    var numNull: Int? = null
+
+
+    //类型检测及自动类型转换
+    fun getStringLength(obj: Any): Int? {
+        //我们可以使用 is 运算符检测一个表达式是否某类型的一个实例(类似于Java中的instanceof关键字)。
+        if (obj is String) {
+            // 做过类型判断以后，obj会被系统自动转换为String类型
+            return obj.length
+        }
+
+        //在这里还有一种方法，与Java中instanceof不同，使用!is
+        // if (obj !is String){
+        //   // XXX
+        // }
+
+        // 这里的obj仍然是Any类型的引用
+        return null
+    }
+
+    fun getStringLength2(obj: Any): Int? {
+        if (obj !is String)
+            return null
+        // 在这个分支中, `obj` 的类型会被自动转换为 `String`
+        return obj.length
+    }
+
+
+    fun getStringLength3(obj: Any): Int? {
+        // 在 `&&` 运算符的右侧, `obj` 的类型会被自动转换为 `String`
+        if (obj is String && obj.length > 0)
+            return obj.length
+        return null
+    }
+
+    fun qujian() {
+        //  区间
+        //  区间表达式由具有操作符形式 .. 的 rangeTo 函数辅以 in 和 !in 形成。
+        //  区间是为任何可比较类型定义的，但对于整型原生类型，它有一个优化的实现。以下是使用区间的一些示例:
+
+        for (i in 1..4) print(i) // 输出“1234”
+
+        for (i in 4..1) print(i) // 什么都不输出
+        var x = 9
+        if (x in 1..10) { // 等同于 1 <= i && i <= 10
+            println(x)
+        }
+
+// 使用 step 指定步长
+        for (i in 1..4 step 2) print(i) // 输出“13”
+
+        for (i in 4 downTo 1 step 2) print(i) // 输出“42”
+
+
+// 使用 until 函数排除结束元素
+        for (i in 1 until 10) {   // i in [1, 10) 排除了 10
+            println(i)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/michaeltung/askotlindemo/l2.kt
@@ -0,0 +1,115 @@
+package com.example.michaeltung.askotlindemo
+
+public  class l2{
+    //定义常量
+    //可以使用下换线使数字常量更 易读
+    val oneMillion = 1_000_000
+    val creditCardNumber = 1234_5678_9012_3456L
+    val socialSecurityNumber = 999_99_9999L
+    val hexBytes = 0xFF_EC_DE_5E
+    val bytes = 0b11010010_01101001_10010100_10010010
+
+    //比较两个数字
+    fun l21() {
+        val a: Int = 10000
+        println(a === a) // true，值相等，对象地址相等  三个等号 === 表示比较对象地址
+
+        //经过了装箱，创建了两个不同的对象
+        val boxedA: Int? = a
+        val anotherBoxedA: Int? = a
+
+        //虽然经过了装箱，但是值是相等的，都是10000
+        println(boxedA === anotherBoxedA) //  false，值相等，对象地址不一样 三个等号 === 表示比较对象地址
+        println(boxedA == anotherBoxedA) // true，值相等 两个 == 表示比较两个值大小
+    }
+
+    //类型转换
+    fun l22() {
+        val b: Byte = 1 // OK, 字面值是静态检测的
+        // val i: Int = b // 错误
+        val i: Int = b.toInt() // OK
+        //每种数据类型都有下面的这些方法，可以转化为其它的类型：
+        /*
+         toByte(): Byte
+         toShort(): Short
+         toInt(): Int
+         toLong(): Long
+         toFloat(): Float
+         toDouble(): Double
+         toChar(): Char
+     */
+        //隐式转换
+        val l = 1L + 3 // Long + Int => Long
+
+//位操作符
+//对于Int和Long类型，还有一系列的位操作符可以使用
+
+        fun weicaozuo() {
+            var a: Int = 4
+            var shl: Int = a shl (1)//左移位 (Java’s <<)
+            var shr: Int = a shr (1) //右移位 (Java’s >>)
+            var ushr: Int = a ushr (1) //无符号右移位 (Java’s >>>)
+            var and: Int = a and (1) //与
+            var or: Int = a or (1)//或
+            var xor: Int = a xor (1)//异域
+            var inv: Int = a.inv()
+
+
+        }
+    }
+
+
+    //字符
+//和 Java 不一样，Kotlin 中的 Char 不能直接和数字操作，Char 必需是单引号 ' 包含起来的。比如普通字符 '0'，'a'。
+//    fun check(c: Char) {
+//        if (c == 1) { // 错误：类型不兼容
+//            // ……
+//        }
+//    }
+//字符字面值用单引号括起来: '1'。 特殊字符可以用反斜杠转义。 支持这几个转义序列：\t、 \b、\n、\r、\'、\"、\\ 和 \$。
+//编码其他字符要用 Unicode 转义序列语法：'\uFF00'。
+//我们可以显式把字符转换为 Int 数字：
+    fun decimalDigitValue(c: Char): Int {
+        if (c !in '0'..'9')
+            throw IllegalArgumentException("Out of range")
+        return c.toInt() - '0'.toInt() // 显式转换为数字
+    }
+
+//布尔  布尔用 Boolean 类型表示，它有两个值：true 和 false。
+//    || – 短路逻辑或
+//    && – 短路逻辑与
+//    ! - 逻辑非
+
+    //数组
+    fun arraydemo() {
+        //[1,2,3]
+        val a = arrayOf(1, 2, 3)
+        //[0,2,4]
+        val b = Array(3, { i -> (i * 2) })
+//读取数组内容
+        println(a[0])//输出结果:1
+        println(b[1])//输出结果:2
+
+        val x: IntArray = intArrayOf(1, 2, 3)
+        x[0] = x[1] + x[2]
+    }
+//字符串
+    //Kotlin 支持三个引号 """ 扩起来的字符串，支持多行字符串
+
+    fun stringDemo() {
+        val text = """
+            多行字符串
+            多行字符串
+            """
+        println(text)   // 输出有一些前置空格
+        val text2 = """
+            |多行字符串
+            |菜鸟教程
+            |多行字符串
+            |Runoob
+            """.trimMargin()
+        //默认 | 用作边界前缀，但你可以选择其他字符并作为参数传入，比如 trimMargin(">")。
+        println(text2)    // 前置空格删除了
+    }
+    //字符串模板
+}

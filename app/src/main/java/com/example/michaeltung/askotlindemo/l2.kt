5e62faa13e061d01a35f846ec43759a9c7d53326
Michael Tung <Michael Tung@MICHAEL-N56>
2018-04-13 15:09:25 +0800

    第二课

--- app/src/main/java/com/example/michaeltung/askotlindemo/l2.kt
+++ app/src/main/java/com/example/michaeltung/askotlindemo/l2.kt
@@ -112,4 +112,28 @@ public  class l2{
         println(text2)    // 前置空格删除了
     }
     //字符串模板
+    fun zifumoban(){
+        val i=10
+        //字符串可以包含模板表达式 ，即一些小段代码，会求值并把结果合并到字符串中。
+        //模板表达式以美元符（$）开头，由一个简单的名字构成:
+        val s="i = $i"//求值结果为:"i = 10"
+        println(s)
+        //或者用花括号扩起来的任意表达式:
+        val s1="runoob"
+        val str="$s.length is ${s.length}"//求值结果为:"runoob.length is 6"
+        println(str)
+        //原生字符串和转义字符串内部都支持模板。
+        // 如果你需要在原生字符串中表示字面值 $ 字符（它不支持反斜杠转义），你可以用下列语法：
+        val price="""
+            ${'$'}9.99
+            """
+        println(price)//求值结果为 $9.99
+
+
+
+    }
+
+
 }
+
+
